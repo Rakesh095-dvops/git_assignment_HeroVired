30f046883c3f83b3b5881cbabd9e1dcc6d0eb08e
Rakesh095-dvops <crashrik89@gmail.com>
2025-02-25 23:13:36 +0530

    added fix in divide function

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -1,17 +1,19 @@
 import math
 class Calculator:
 
-def add(self, a, b):
-    return a + b
+    def add(self, a, b):
+        return a + b
 
-def subtract(self, a, b):
-    return a - b
+    def subtract(self, a, b):
+        return a - b
 
-def multiply(self, a, b):
-    return a * b
+    def multiply(self, a, b):
+        return a * b
 
-def divide(self, a, b):
-    return a / b
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
+        return a / b
 
 # TODO: Implement the following function to calculate the square root of a number.
 # def square_root(self, x):
@@ -21,7 +23,8 @@ def divide(self, a, b):
 if __name__ == "__main__":
     calculator = Calculator()
     num1 = 16
-    num2 = 4
+    #num2 = 4
+    num2 = 0
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
