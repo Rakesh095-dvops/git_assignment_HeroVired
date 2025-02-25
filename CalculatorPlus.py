d9da69c8db03215172adb760687b4c93ef938ab7
Rakesh095-dvops <crashrik89@gmail.com>
2025-02-26 00:26:33 +0530

    Exception working fine Commmmenting test scenario

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -21,8 +21,8 @@ class Calculator:
 if __name__ == "__main__":
     calculator = Calculator()
     num1 = 16
-    #num2 = 4
-    num2 = 0
+    num2 = 4
+    #num2 = 0
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
